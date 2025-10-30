2cc3f8ac4f91d89681797e2874c0ad77452e16db
Lk111111111 <narbex112@gmail.com>
2025-10-30 15:27:14 +0100

    First scripting using the free api of freeaudiodb

--- scripts/main.py
+++ scripts/main.py
@@ -1,3 +1,10 @@
-print("hi test")
+import requests
+import json
+API_URL = "https://www.theaudiodb.com/api/v1/json"
+API_test_key = "/123"
+API_URL_with_key = API_URL+API_test_key
 
-
+request = requests.get("https://www.theaudiodb.com/api/v1/json/123/search.php?s=coldplay")
+with open("response.json", "w", encoding="UTF-8") as r:
+    json_dumped = json.dumps(request.json())
+    r.write(json_dumped)
