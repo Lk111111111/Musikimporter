b437c8721b4221e05aaee16531f6de08956759d4
Lk111111111 <narbex112@gmail.com>
2025-10-30 15:47:40 +0100

    Added first functionality to create some artist jsons

--- scripts/main.py
+++ scripts/main.py
@@ -4,7 +4,19 @@ API_URL = "https://www.theaudiodb.com/api/v1/json"
 API_test_key = "/123"
 API_URL_with_key = API_URL+API_test_key
 
-request = requests.get("https://www.theaudiodb.com/api/v1/json/123/search.php?s=coldplay")
-with open("response.json", "w", encoding="UTF-8") as r:
-    json_dumped = json.dumps(request.json())
-    r.write(json_dumped)
+
+def decorator(function):
+    def wrapper(*args, **kwargs):
+        function(artist)
+        print("artist file created")
+    return wrapper
+
+@decorator
+def get_artist_generell_info(artist: str):
+    request = requests.get(f"https://www.theaudiodb.com/api/v1/json/123/search.php?s={artist}")
+    with open(f"{artist}.json", "w", encoding="UTF-8") as r:
+        json_dumped = json.dumps(request.json())
+        r.write(json_dumped)
+
+artist = input("Enter the artist to search for: ")
+get_artist_generell_info(artist.strip())
